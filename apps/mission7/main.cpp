b4f2a9227e633769bb22133ccee7e0597f6717d0
PntAndCnt <peanutandchestnut@gmail.com>
2018-05-17 08:42:37 +0800

    use boost program options.

--- apps/mission7/main.cpp
+++ apps/mission7/main.cpp
@@ -5,38 +5,53 @@
 #include "components/vfs/bsaarchive.hpp"
 #include <osgViewer/Viewer>
 #include <osgViewer/ViewerEventHandlers>
+#include <boost/program_options.hpp>
 
 char nonstrict_normalize_char(char ch)
 {
     return ch == '\\' ? '/' : Misc::StringUtils::toLower(ch);
 }
 
+struct Argument{
+    std::string file;
+    std::string name;
+};
+
 int main(int argc, char *argv[])
 {
-    if (argc != 3) {
-        std::cout << "it should be " << argv[0] << " bsa modelName" << std::endl;
-        return 0;
-    }
+    Argument arg;
+    namespace bpo = boost::program_options;
+    bpo::options_description desc;
+    desc.add_options()
+        ("file", bpo::value<std::string>(&arg.file), "data file, otherwise use $BSA_DATA")
+        ("name", bpo::value<std::string>(&arg.name), "record name")
+        ;
 
-    std::string filename(argv[1]);
-    std::string modelName(argv[2]);
+    bpo::positional_options_description pod;
+    pod.add("file", 1);
+
+    bpo::variables_map vm;
+
+    bpo::store(bpo::command_line_parser(argc, argv).options(desc).positional(pod).run(), vm);
+    bpo::notify(vm);
+
     bool strict = false;
 
-    VFS::BsaArchive* bsaArchive = new VFS::BsaArchive(filename);
+    VFS::BsaArchive* bsaArchive = new VFS::BsaArchive(arg.file);
 
     VFS::Manager manager(strict);
     manager.addArchive(bsaArchive);
     manager.buildIndex();
-    
+
     Resource::ResourceSystem resourceSystem(&manager);
 
     auto sceneManager = resourceSystem.getSceneManager();
 
-    osg::ref_ptr<osg::Node> model = sceneManager->getInstance(modelName);
+    osg::ref_ptr<osg::Node> model = sceneManager->getInstance(arg.name);
 
     osgViewer::Viewer viewer;
     viewer.addEventHandler(new osgViewer::StatsHandler);
     viewer.setSceneData(model);
-    
+
     return viewer.run();
 }
